3ec2a4077b999d123f6fdaf6e37a3f87fcde3a32
Brock Ross <m.brock.ross@gmail.com>
2019-04-30 15:43:06 -0600

    newrelic testing setup

--- proxy/async-request.js
+++ proxy/async-request.js
@@ -1,9 +1,17 @@
 const request = require('request')
 
-module.exports = async (url) =>
+module.exports.get = async (url) =>
     new Promise((resolve, reject) => {
         request(url, (error, response, data) => {
             if(error) reject(error)
             else resolve(data)
         })
     })
+
+module.exports.post = async (options) =>
+new Promise((resolve, reject) => {
+    request.post(options, (error, response, data) => {
+        if(error) reject(error)
+        else resolve(data)
+    })
+})
--- proxy/server.js
+++ proxy/server.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
@@ -5,15 +6,27 @@ const app = express();
 const port = process.env.PORT || 3000;
 const request = require('request');
 const asyncRequest = require('./async-request');
+const parser = require('body-parser');
 
 
 app.use(morgan('dev'));
+app.use(parser());
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/:id', express.static(path.join(__dirname, 'public')));
 
 app.get('/support/:id', async (req, res) => {
   const albumId = req.params.id;
-  const supportData = await asyncRequest(`http://localhost:3003/support/${albumId}`);
+  const supportData = await asyncRequest.get(`http://localhost:3003/support/${albumId}`);
+  res.send(supportData);
+})
+
+app.post('/support', async (req, res) => {
+  const commentsData = req.body;
+  const supportData = await asyncRequest.post({
+    headers: {'content-type': 'application/json'},
+    url: 'http://localhost:3003/support',
+    form: commentsData
+});
   res.send(supportData);
 })
 
